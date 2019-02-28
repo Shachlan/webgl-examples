73f5369467f397a691809650b8e7f3b634f532ab
Shachar Langbeheim <shachar@lightricks.com>
2019-02-28 10:22:37 +0200

    remove usage of unwanted variables in shaders.

--- tutorial/sample8/webgl-demo.js
+++ tutorial/sample8/webgl-demo.js
@@ -24,8 +24,8 @@ function main() {
 
   const vsSource = `
     attribute vec4 aVertexPosition;
-    attribute vec3 aVertexNormal;
     attribute vec2 aTextureCoord;
+    attribute vec3 aVertexNormal;
 
     uniform mat4 uNormalMatrix;
     uniform mat4 uModelViewMatrix;
@@ -35,19 +35,13 @@ function main() {
     varying highp vec3 vLighting;
 
     void main(void) {
-      gl_Position = uProjectionMatrix * uModelViewMatrix * aVertexPosition;
+      uNormalMatrix;
+      uProjectionMatrix;
+      uModelViewMatrix;
+      vLighting;
+      aVertexNormal;
+      gl_Position = aVertexPosition;
       vTextureCoord = aTextureCoord;
-
-      // Apply lighting effect
-
-      highp vec3 ambientLight = vec3(0.3, 0.3, 0.3);
-      highp vec3 directionalLightColor = vec3(1, 1, 1);
-      highp vec3 directionalVector = normalize(vec3(0.85, 0.8, 0.75));
-
-      highp vec4 transformedNormal = uNormalMatrix * vec4(aVertexNormal, 1.0);
-
-      highp float directional = max(dot(transformedNormal.xyz, directionalVector), 0.0);
-      vLighting = ambientLight + (directionalLightColor * directional);
     }
   `;
 
@@ -60,9 +54,9 @@ function main() {
     uniform sampler2D uSampler;
 
     void main(void) {
-      highp vec4 texelColor = texture2D(uSampler, vTextureCoord);
+      vLighting;
 
-      gl_FragColor = vec4(texelColor.rgb * vLighting, texelColor.a);
+      gl_FragColor = texture2D(uSampler, vTextureCoord);
     }
   `;
 
