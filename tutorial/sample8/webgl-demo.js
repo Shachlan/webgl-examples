eb6110a86ab3b03d8afddc65cad83248fa463048
Shachar Langbeheim <shachar@lightricks.com>
2019-03-12 10:24:23 +0200

    print fps on end

--- tutorial/sample8/webgl-demo.js
+++ tutorial/sample8/webgl-demo.js
@@ -1,3 +1,63 @@
+/**
+ *
+ * @param {number} fps
+ */
+function createFrameRenderer(fps) {
+  let wasStopped = false;
+  let frameCount = 0;
+  const fpsInterval = 1000 / fps;
+  let now, elapsed;
+  let then = performance.now();
+  let startTime = then;
+  let count,
+    seconds = 0;
+  let drawCallback = () => {
+    throw new Error("no draw callback provided!");
+    return undefined;
+  };
+  let fpsCount = [];
+
+  /**
+   *
+   * @param {() => any} callback - The drawing code.
+   */
+  function render(callback) {
+    drawCallback = callback;
+    renderFrame();
+  }
+
+  function renderFrame() {
+    if (wasStopped) {
+      return;
+    }
+    now = performance.now();
+    elapsed = now - then;
+
+    if (elapsed <= fpsInterval) {
+      requestAnimationFrame(renderFrame);
+      return;
+    }
+    count += 1;
+    if ((now - startTime) / 1000 > seconds) {
+      seconds += 1;
+      fpsCount.push(count);
+      count = 0;
+    }
+    then = now - (elapsed % fpsInterval);
+    drawCallback();
+    requestAnimationFrame(renderFrame);
+  }
+  function stop() {
+    console.log("stopped. FPS was: ", fpsCount);
+    wasStopped = true;
+  }
+
+  return {
+    render,
+    stop
+  };
+}
+
 // will set to true when video can be copied to texture
 var copyVideo = false;
 
@@ -146,8 +206,7 @@ function main() {
 
   const texture1 = initTexture(gl);
 
-  const video1 = setupVideo("race.mp4");
-
+  const video1 = setupVideo("dog.mp4");
   // Draw the scene repeatedly
   function render() {
     if (copyVideo) {
@@ -155,10 +214,11 @@ function main() {
     }
 
     drawScene(gl, programInfo, buffers, texture1);
-
-    requestAnimationFrame(render);
   }
-  requestAnimationFrame(render);
+
+  let frameRenderer = createFrameRenderer(30);
+  video1.onended = frameRenderer.stop;
+  frameRenderer.render(render);
 }
 
 function setupVideo(url) {
@@ -169,7 +229,7 @@ function setupVideo(url) {
 
   video.autoplay = true;
   video.muted = true;
-  video.loop = true;
+  video.loop = false;
 
   // Waiting for these 2 events ensures
   // there is data in the video
